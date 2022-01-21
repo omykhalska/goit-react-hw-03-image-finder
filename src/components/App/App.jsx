de9a942b96c1abfbfb3300ebb3ed3a3a2a906cdf
omykhalska <83216292+omykhalska@users.noreply.github.com>
2022-01-21 14:47:11 +0200

    Добавляет паттерн "State машина"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import { BallTriangle } from 'react-loader-spinner';
 import Searchbar from '../Searchbar/';
 import ImageGallery from '../ImageGallery';
 import Button from '../Button/';
 import { Container } from './App.styled';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { getImages } from '../../services/searchImagesApi';
 
 class App extends Component {
@@ -12,40 +14,48 @@ class App extends Component {
     images: [],
     totalImages: 0,
     page: 1,
-    loading: false,
+    status: 'idle',
   };
 
-  componentDidUpdate(_, prevStates) {
+  async componentDidUpdate(_, prevStates) {
     const { page, query } = this.state;
+    const { notification } = this;
 
     if (prevStates.query !== query) {
-      this.setState({ loading: true });
+      this.setState({ status: 'pending' });
 
-      getImages(query, page)
-        .then(({ data }) => {
-          this.setState({ images: data.hits, totalImages: data.totalHits });
-          // TODO: убрать этот блок, если его нет в ТЗ (ПРОВЕРИТЬ !!!)
-          //   data.hits.length === 0 &&
-          //   toast.error('There is no images for this search query');
-        })
-        .catch(error => console.log(error.message))
-        .finally(() => this.setState({ loading: false }));
-    } else if (prevStates.page !== page) {
-      this.setState({ loading: true });
-
-      getImages(query, page)
+      await getImages(query, page)
         .then(({ data }) => {
           if (data.hits.length === 0) {
-            toast.error('No more images found', { position: 'bottom-center' });
+            notification('There is no images for this search query');
+            this.setState({ status: 'idle', images: [] });
             return;
           }
 
+          this.setState({
+            images: data.hits,
+            totalImages: data.totalHits,
+            status: 'resolved',
+          });
+        })
+        .catch(error => {
+          console.log(error.message);
+          this.setState({ status: 'rejected' });
+        });
+    } else if (prevStates.page !== page) {
+      this.setState({ status: 'pending' });
+
+      await getImages(query, page)
+        .then(({ data }) =>
           this.setState(prevState => ({
             images: [...prevState.images, ...data.hits],
-          }));
-        })
-        .catch(error => console.log(error.message))
-        .finally(() => this.setState({ loading: false }));
+            status: 'resolved',
+          })),
+        )
+        .catch(error => {
+          console.log(error.message);
+          this.setState({ status: 'rejected' });
+        });
     }
   }
 
@@ -57,22 +67,76 @@ class App extends Component {
     this.setState({ query, page: 1 });
   };
 
+  notification = text => {
+    toast.error(text);
+  };
+
   render() {
     const { handleFormSubmit, handleLoadMoreBtn } = this;
-    const { images, totalImages } = this.state;
+    const { images, totalImages, status, page } = this.state;
 
-    return (
-      <Container>
-        <Toaster />
-        <Searchbar onSubmit={handleFormSubmit} />
+    if (status === 'idle') {
+      return (
+        <Container>
+          <Toaster />
+          <Searchbar onSubmit={handleFormSubmit} />
+        </Container>
+      );
+    }
 
-        {images.length > 0 ? <ImageGallery images={images} /> : null}
+    if (status === 'pending') {
+      if (page > 1) {
+        return (
+          <Container>
+            <Toaster />
+            <Searchbar onSubmit={handleFormSubmit} />
+            <ImageGallery images={images} />
+            <BallTriangle
+              heigth="100"
+              width="100"
+              color="grey"
+              arialLabel="loading-indicator"
+            />
+          </Container>
+        );
+      } else {
+        return (
+          <Container>
+            <Toaster />
+            <Searchbar onSubmit={handleFormSubmit} />
+            <BallTriangle
+              heigth="100"
+              width="100"
+              color="grey"
+              arialLabel="loading-indicator"
+            />
+          </Container>
+        );
+      }
+    }
 
-        {images.length > 0 && totalImages !== images.length ? (
-          <Button onClick={handleLoadMoreBtn} />
-        ) : null}
-      </Container>
-    );
+    if (status === 'rejected') {
+      return (
+        <Container>
+          <Toaster />
+          <Searchbar onSubmit={handleFormSubmit} />
+          <h1>Something went wrong... Try again later!</h1>
+        </Container>
+      );
+    }
+
+    if (status === 'resolved') {
+      return (
+        <Container>
+          <Toaster />
+          <Searchbar onSubmit={handleFormSubmit} />
+          <ImageGallery images={images} />
+          {images.length > 0 && totalImages !== images.length ? (
+            <Button onClick={handleLoadMoreBtn} />
+          ) : null}
+        </Container>
+      );
+    }
   }
 }
 
